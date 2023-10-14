7fb6e4a6dfeb28c3cbd6eca49e1a4c497b3fbfb9
Ansear <sebastianarciniegas2003@gmail.com>
2023-10-14 11:21:06 -0500

    feat: :wrench: Add Ciudad Controller

--- API/Controllers/CiudadController.cs
+++ API/Controllers/CiudadController.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Dtos;
 using AutoMapper;
+using Core.Entities;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 public class CiudadController : BaseController
@@ -15,5 +14,71 @@ public class CiudadController : BaseController
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<IEnumerable<CiudadDto>>> Get()
+    {
+        var ciudades = await _unitOfWork.Ciudades.GetAllAsync();
+        return _mapper.Map<List<CiudadDto>>(ciudades);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<CiudadDto>> Get(int id)
+    {
+        var ciudad = await _unitOfWork.Ciudades.GetByIdAsync(id);
+        if(ciudad == null)
+            return NotFound();
+        return _mapper.Map<CiudadDto>(ciudad);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Ciudad>> Post([FromBody] CiudadDto ciudadDto)
+    {
+        var ciudad = _mapper.Map<Ciudad>(ciudadDto);
+        _unitOfWork.Ciudades.Add(ciudad);
+        await _unitOfWork.SaveAsync();
+        if(ciudad == null)
+            return BadRequest();
+        ciudadDto.Id = ciudad.Id;
+        return ciudad;
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<CiudadDto>> Put(int id, [FromBody] CiudadDto ciudadDto)
+    {
+        if(ciudadDto == null)
+            return BadRequest();
+        if(ciudadDto.Id == 0)
+            ciudadDto.Id = id;
+        if(ciudadDto.Id != id)
+            return NotFound();
+        var ciudad = _mapper.Map<Ciudad>(ciudadDto);
+        _unitOfWork.Ciudades.Update(ciudad);
+        await _unitOfWork.SaveAsync();
+        return ciudadDto;
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var ciudad = await _unitOfWork.Ciudades.GetByIdAsync(id);
+        if(ciudad == null)
+            return NotFound();
+        _unitOfWork.Ciudades.Remove(ciudad);
+        await _unitOfWork.SaveAsync();
+        return NoContent();
     }
 }
